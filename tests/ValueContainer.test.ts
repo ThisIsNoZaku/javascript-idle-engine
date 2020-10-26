a8f07ea217f836f927f4bbca9ef4e5f5dcb24b5b
Damie <damienmarble@gmail.com>
2020-10-26 18:21:13 -0400

    Don't intercept properties starting with '$'.

--- tests/ValueContainer.test.ts
+++ tests/ValueContainer.test.ts
@@ -1,32 +1,22 @@
-import {ValueContainer} from "../src/ValueContainer";
+import {changeListeners, ValueContainer} from "../src/ValueContainer";
 import {Engine} from "../src/Engine";
 import {EngineConfiguration} from "../src";
 import * as ts from "typescript/lib/tsserverlibrary";
+import {tokenToString} from "typescript/lib/tsserverlibrary";
 
-describe("ValueContainer", function () {
+describe("ValueContainer wrapping a primitive", function () {
     let engine: Engine;
     beforeEach(() => engine = new Engine(new EngineConfiguration()));
-    it("can wrap a primitive value which can be retrieved by get", function () {
-        const stringRef = ValueContainer(1, <Engine>(<unknown>null), {
-            startingValue: "string"
-        });
-        const numRef = ValueContainer(1, <Engine>(<unknown>null), {
-            startingValue: 1
-        });
-        const boolRef = ValueContainer(1, <Engine>(<unknown>null), {
-            startingValue: true
-        });
-        expect(stringRef.get()).toEqual("string");
-        expect(numRef.get()).toEqual(1);
-        expect(boolRef.get()).toEqual(true);
+    it("can wrap an object or primitive", function () {
+        const ref = ValueContainer(1, <Engine>(<unknown>null), {startingValue: {}});
+        expect(ref).toMatchObject({});
     });
-    it("can change the wrapped value via set", function () {
-        const ref = ValueContainer(1, <Engine>(<unknown>null), {});
-        expect(ref.get()).toBeUndefined();
-        ref.set(123);
-        expect(ref.get()).toBe(123);
+    it("throws an error when trying to wrap a primitive", function () {
+        expect(() => ValueContainer(1, <Engine>(<unknown>null), {startingValue: 1})).toThrow();
+        expect(() => ValueContainer(1, <Engine>(<unknown>null), {startingValue: true})).toThrow();
+        expect(() => ValueContainer(1, <Engine>(<unknown>null), {startingValue: "string"})).toThrow();
     });
-    it("wraps all child values in an container", function () {
+    it("wraps all child values", function () {
         const ref = ValueContainer(1, engine, {
             startingValue: {
                 string: {startingValue: "string"},
@@ -34,34 +24,56 @@ describe("ValueContainer", function () {
                 boolean: {startingValue: true}
             }
         });
-        expect(ref.string.get()).toEqual("string");
-        expect(ref.number.get()).toEqual(1);
-        expect(ref.boolean.get()).toEqual(true);
+        expect(ref.string).toEqual("string");
+        expect(ref.number).toEqual(1);
+        expect(ref.boolean).toEqual(true);
     });
-    it("can be subscribed to", function () {
-        const ref = ValueContainer(1, engine, {startingValue: "string"});
-        ref.on("changed", function (arg: any) {
-            expect(arg).toEqual("new");
+    it("properties of the object can be subscribed to", function () {
+        const ref = ValueContainer(1, engine, {startingValue: {}});
+        const listener = jest.fn();
+        ref.watch(listener);
+        ref.foo = "new";
+        expect(listener).toHaveBeenCalledWith("foo", "new");
+    });
+    it("when a child object changes, that objects parents are notified as well", function () {
+        const ref = ValueContainer(1, engine, {
+            startingValue: {
+                child: {
+                    startingValue: {}
+                }
+            }
         });
-        ref.set("new");
-        expect.hasAssertions();
+        const parentListener = jest.fn();
+        ref.watch(parentListener);
+        ref.child.property = "123";
+        expect(parentListener.mock.calls[0][0]).toBe("child")
+        expect(parentListener.mock.calls[0][1].child.property).toBe("123");
     });
     it("subscriptions to the container can be removed", function () {
-        const ref = ValueContainer(1, engine, {startingValue: "string"});
+        const ref = ValueContainer(1, engine, {startingValue: {}});
         const changeCallback = jest.fn();
-        const subscription = ref.on("changed", changeCallback);
+        const subscription = ref.watch(changeCallback);
         subscription.unsubscribe();
-        ref.set(123);
+        ref.property = 123;
         expect(changeCallback).not.toHaveBeenCalled();
-    })
-    it("setting a property on an object notifies the object's listeners", function () {
-        const ref = ValueContainer(1, engine, {});
-        const changeCallback = jest.fn();
-        ref.on("changed", changeCallback);
-        ref.foo.set(123);
-        expect(changeCallback.mock.calls.length).toBe(1);
     });
+    it("if a child object is replaced, the parent no longer receives change notifications about it", function () {
+        const ref = ValueContainer(1, engine, {startingValue: {
+            child: {
+                startingValue: {}
+            }
+            }});
+        const changeCallback = jest.fn();
+        const firstChild = ref.child;
+        ref.child.watch(changeCallback);
+        ref.child = {};
+        ref.child.foo = "bar";
+        expect(changeCallback).not.toHaveBeenCalled();
+        expect(firstChild[changeListeners].length).toBe(1);
+    });
+    it("if an object is assigned, it is wrapped", function () {
 
+    });
 });
 
 describe("array ValueContainer", function () {
@@ -80,42 +92,40 @@ describe("array ValueContainer", function () {
         const ref = engine.createReference({
             startingValue: []
         });
-        expect([...ref.get()]).toEqual([]);
+        expect([...ref]).toEqual([]);
     });
     it("assigning to an index wraps the value", function () {
         const ref = engine.createReference({
             startingValue: []
         });
-        ref.get()[0] = 123
-        expect(ref.get()[0]).toBe(123);
+        ref[0] = 123
+        expect(ref[0]).toBe(123);
     });
     it("inserting an object via push wraps the value", function () {
         const ref = engine.createReference({startingValue: []});
         ref.push(123);
-        expect(ref[0].get()).toBe(123);
+        expect(ref[0]).toBe(123);
     });
     it("assigning to an index that already has a wrapped value assigns to the wrapper instead of creating a new one", function () {
         const ref = engine.createReference({startingValue: [123]});
-        const original = ref[0];
-        ref[0].set(321);
-        expect(ref.get()[0] === original).toBeTruthy();
+        ref[0] = 321;
+        expect([...ref]).toMatchObject([321])
     });
     it("modifying an object notifies all parent listeners", function () {
         const topCallback = jest.fn();
         const middleCallback = jest.fn();
-        const bottomCallback = jest.fn();
-        engine.globals.top.on("changed", topCallback);
-        engine.globals.top.middle.on("changed", middleCallback);
-        engine.globals.top.middle.get().bottom.on("changed", bottomCallback);
-        engine.globals.top.middle.bottom.set(123);
+        engine.globals.top.watch(topCallback);
+        engine.globals.top.middle.watch(middleCallback);
+        engine.globals.top.middle.bottom = 123;
 
-        // FIXME: Find a way to test arguments are correct. The ValueContainer proxy causes problems with asserting.
-        expect(bottomCallback.mock.calls.length).toBe(1);
         expect(middleCallback.mock.calls.length).toBe(1);
+        expect(middleCallback.mock.calls[0]).toEqual(["bottom", 123]);
         expect(topCallback.mock.calls.length).toBe(1);
+        expect(topCallback.mock.calls[0][0]).toEqual("middle");
+        expect(topCallback.mock.calls[0][1].middle.bottom).toEqual(123);
     });
 
-    it("modifying an object notifies all parent listeners", function () {
+    it("modifying an array notifies all parent listeners", function () {
         engine = new Engine(new EngineConfiguration()
             .WithGlobalProperties({
                     top:
@@ -130,20 +140,19 @@ describe("array ValueContainer", function () {
         const middleCallback = jest.fn();
         const bottomCallback = jest.fn();
         const absoluteBottonCallback = jest.fn();
-        engine.globals.top.on("changed", topCallback);
-        engine.globals.top[0].on("changed", middleCallback);
-        engine.globals.top[0][0].on("changed", bottomCallback);
-        engine.globals.top[0][0][0].on("changed", absoluteBottonCallback);
-        engine.globals.top[0][0][0].set(1);
+        engine.globals.top.watch(topCallback);
+        engine.globals.top[0].watch(middleCallback);
+        engine.globals.top[0][0].watch(bottomCallback);
+        engine.globals.top[0][0][0] = 1;
 
         expect(bottomCallback.mock.calls.length).toBe(1);
         expect(middleCallback.mock.calls.length).toBe(1);
         expect(topCallback.mock.calls.length).toBe(1);
     });
     it("calling update calls update on children", function () {
-        const topUpdater = jest.fn().mockReturnValue(null);
-        const middleUpdater = jest.fn().mockReturnValue(null);
-        const bottomUpdater = jest.fn().mockReturnValue(null);
+        const topUpdater = jest.fn(arg => arg);
+        const middleUpdater = jest.fn(arg => arg);
+        const bottomUpdater = jest.fn(arg => arg);
         engine = new Engine(new EngineConfiguration()
             .WithGlobalProperties({
                     top: EngineConfiguration.configProperty({
